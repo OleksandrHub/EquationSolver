877a6580cea219d525d5f2017f7128d482ef5aff
OleksandrHub <sasha.klishch1@gmail.com>
2025-02-19 20:12:18 +0200

    feat(equation): enhance equation solver
    - Added 'type' attribute to Equation class
    - Improved LinearEquation and QuadraticEquation
    - Implemented detailed output in solver
    - Handle different equation solution types
    - Added comprehensive analysis for QuadraticEquation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 
 class Equation(ABC):
     degree: int
+    type: str
 
     def __init__(self, *args):
         if (self.degree + 1) != len(args):
@@ -20,6 +21,10 @@ class Equation(ABC):
         if not hasattr(cls, "degree"):
             raise AttributeError(
                 f"Cannot create '{cls.__name__}' class: missing required attribute 'degree'"
+            )
+        if not hasattr(cls, "type"):
+            raise AttributeError(
+                f"Cannot create '{cls.__name__}' class: missing required attribute 'type'"
             )
 
     def __str__(self):
@@ -44,8 +49,10 @@ class Equation(ABC):
     def analyze(self):
         pass
 
+
 class LinearEquation(Equation):
     degree = 1
+    type = 'Linear Equation'
 
     def solve(self):
         a, b = self.coefficients.values()
@@ -56,8 +63,10 @@ class LinearEquation(Equation):
         slope, intercept = self.coefficients.values()
         return {'slope': slope, 'intercept': intercept}
 
+
 class QuadraticEquation(Equation):
     degree = 2
+    type = 'Quadratic Equation'
 
     def __init__(self, *args):
         super().__init__(*args)
@@ -67,13 +76,14 @@ class QuadraticEquation(Equation):
     def solve(self):
         if self.delta < 0:
             return []
-        a, b, c = self.coefficients.values()
-        x1 = (-b + self.delta ** 0.5) / (2 * a)
-        x2 = (-b - self.delta ** 0.5) / (2 * a)
+        a, b, _ = self.coefficients.values()
+        x1 = (-b + (self.delta) ** 0.5) / (2 * a)
+        x2 = (-b - (self.delta) ** 0.5) / (2 * a)
         if self.delta == 0:
             return [x1]
+
         return [x1, x2]
-        
+
     def analyze(self):
         a, b, c = self.coefficients.values()
         x = -b / (2 * a)
@@ -84,12 +94,37 @@ class QuadraticEquation(Equation):
         else:
             concavity = 'downwards'
             min_max = 'max'
-
         return {'x': x, 'y': y, 'min_max': min_max, 'concavity': concavity}
 
-def solver(equation):
-        if not isinstance(equation, Equation):
-            raise TypeError("Argument must be an Equation object")
 
+def solver(equation):
+    if not isinstance(equation, Equation):
+        raise TypeError("Argument must be an Equation object")
+
+    output_string = f'\n{equation.type:-^24}'
+    output_string += f'\n\n{equation!s:^24}\n\n'
+    output_string += f'{"Solutions":-^24}\n\n'
+    results = equation.solve()
+    match results:
+        case []:
+            result_list = ['No real roots']
+        case [x]:
+            result_list = [f'x = {x:+.3f}']
+        case [x1, x2]:
+            result_list = [f'x1 = {x1:+.3f}', f'x2 = {x2:+.3f}']
+    for result in result_list:
+        output_string += f'{result:^24}\n'
+    output_string += f'\n{"Details":-^24}\n\n'
+    details = equation.analyze()
+    match details:
+        case {'slope': slope, 'intercept': intercept}:
+            details_list = [f'slope = {slope:>16.3f}', f'y-intercept = {intercept:>10.3f}']
+        case {'x': x, 'y': y, 'min_max': min_max, 'concavity': concavity}:
+            coord = f'({x:.3f}, {y:.3f})'
+            details_list = [f'concavity = {concavity:>12}', f'{min_max} = {coord:>18}']
+    for detail in details_list:
+        output_string += f'{detail}\n'
+    return output_string
 lin_eq = LinearEquation(2, 3)
 quadr_eq = QuadraticEquation(1, 2, 1)
+print(solver(lin_eq))
